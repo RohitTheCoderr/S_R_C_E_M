cfbad9e9ddad813f134b9283bbb2d42dfd8d4fda
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2024-12-17 13:28:21 +0530

    navbar completed

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,11 +1,10 @@
 import Navbar from "../navbar/Navbar"
-
 const Header = ({img}) => {
   return (
     <>
-    <div className="h-[4.5rem] shadow-lg bg-slate-50 w-screen text-center flex justify-around items-center mb-3 ">
-      <div className="h-[3rem] ml-[-2.5rem] w-[10rem] flex justify-center items-center cursor-pointer">
-        <img src="https://ruloans-bucket.s3.amazonaws.com/b2c/assets/logo.webp" alt="" className="h-[2.2rem] w-[10rem]" />
+    <div className="h-[6rem] shadow-lg bg-slate-50 w-screen text-center flex justify-around items-center mb-3 ">
+      <div className=" w-[5.5rem] flex justify-center items-center cursor-pointer">
+        <img src="/images/common/image.png" alt="" className=" w-full" />
       </div>
       <Navbar/>
       <div className="bg-red-100 hover:bg-red-200 text-red-600 font-semibold capitalize rounded-full px-4 py-2 ml-12 mr-[-3rem] cursor-pointer">sign in</div>
