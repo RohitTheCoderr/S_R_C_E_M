d37c1fd8c9bb8c216a34ea5fd2a13cdce1be09dc
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2024-12-17 18:26:35 +0530

    footer component completed

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({img}) => {
         <img src="/images/common/image.png" alt="" className=" w-full rounded-full" />
       </div>
       <Navbar/>
-      <div className="bg-red-100 hover:bg-red-200 text-red-600 font-semibold capitalize rounded-full px-4 py-2 ml-12 mr-[-3rem] cursor-pointer">sign in</div>
+      {/* <div className="bg-red-100 hover:bg-red-200 text-red-600 font-semibold capitalize rounded-full px-4 py-2 ml-12 mr-[-3rem] cursor-pointer">sign in</div> */}
     </div> 
    </>
   )
