535ad62d1e862ceb66e9a0cfc49d651d13f09da0
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2024-12-20 14:55:33 +0530

    now navbar is responsive for all device

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,15 +1,40 @@
 import Navbar from "../navbar/Navbar";
-const Header = ({img}) => {
+
+const Header = ({ img }) => {
   return (
     <>
-    <div className="h-[6rem] shadow-lg bg-slate-50 w-full text-center flex justify-around items-center">
-      <div className=" w-[5.5rem] flex justify-center items-center cursor-pointer">
-        <img src="/images/common/image.png" alt="" className=" w-full rounded-full" />
+      <div className="h-[7rem]  sm:h-[6rem] shadow-lg w-full text-center flex-row sm:flex justify-between items-center px-4 lg:px-10">
+        <div className="w-[3.8rem] relative my-3  sm:w-[5rem] md:w-[5.5rem] flex justify-center items-center cursor-pointer">
+          <img
+            src={img || "/images/common/image.png"}
+            alt="Logo"
+            className="w-full rounded-full"
+          />
+        </div>
+        <Navbar />
       </div>
-      <Navbar/>
-    </div> 
-   </>
-  )
-}
+    </>
+  );
+};
 
-export default Header
+export default Header;
+
+
+
+// import Navbar from "../navbar/Navbar";
+// const Header = ({img}) => {
+//   return (
+//     <>
+//     <div className="h-[6rem] shadow-lg bg-slate-50 w-full text-center flex justify-around items-center">
+//       <div className=" w-[5.5rem] flex justify-center items-center cursor-pointer">
+//         <img src="/images/common/image.png" alt="" className=" w-full rounded-full" />
+//       </div>
+//       <Navbar/>
+//     </div> 
+//    </>
+//   )
+// }
+
+// export default Header
+
+
