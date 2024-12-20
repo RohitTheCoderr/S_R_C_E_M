71199145a0f9d2c24e0c4d0d9d501df3e179bb79
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2024-12-20 15:03:31 +0530

    navbar update

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -19,22 +19,3 @@ const Header = ({ img }) => {
 
 export default Header;
 
-
-
-// import Navbar from "../navbar/Navbar";
-// const Header = ({img}) => {
-//   return (
-//     <>
-//     <div className="h-[6rem] shadow-lg bg-slate-50 w-full text-center flex justify-around items-center">
-//       <div className=" w-[5.5rem] flex justify-center items-center cursor-pointer">
-//         <img src="/images/common/image.png" alt="" className=" w-full rounded-full" />
-//       </div>
-//       <Navbar/>
-//     </div> 
-//    </>
-//   )
-// }
-
-// export default Header
-
-
