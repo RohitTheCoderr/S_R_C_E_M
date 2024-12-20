abf326d7753b2c6a2475e9e6a4bca61e68925215
Jaya <jayanigam17@gmail.com>
2024-12-20 21:35:06 +0530

    commite update page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './layouts/header/Header';
-import Footer from './layouts/footer/Footer';
+// import Footer from './layouts/footer/Footer';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
