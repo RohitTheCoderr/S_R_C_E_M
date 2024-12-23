90de629da95713ecbbb37472db5848f9ffde5d7c
Jaya <jayanigam17@gmail.com>
2024-12-23 10:03:00 +0530

    Procedure & Eligibility Criteria completd in admission page

--- src/components/admission/Procedure.jsx
+++ src/components/admission/Procedure.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Procedure() {
   return (
-<>
-<h1> heelo mr Admission</h1>
-</>
-  )
+    <>
+      <div className="h-screen w-screen px-4 py-2 md:py-4 md:px-8 lg:py-8 lg:px-16">
+        <iframe
+          src="src\components\admission\Procedure_&_Eligibilty_Criteria.pdf"
+          className="h-full w-full rounded-md shadow-lg"
+        ></iframe>
+      </div>
+    </>
+  );
 }
 
-export default Procedure
+export default Procedure;
